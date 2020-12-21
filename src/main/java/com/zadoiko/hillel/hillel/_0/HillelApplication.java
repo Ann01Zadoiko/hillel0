13539f18987557b893e36e21a9a8f97e8dc3aeaa
Ann01Zadoiko <anna.zadoiko@gmail.com>
2020-12-21 19:08:55 +0200

    Home work 28

---
+++ src/main/java/com/zadoiko/hillel/hillel/_0/HillelApplication.java
@@ -0,0 +1,13 @@
+package com.zadoiko.hillel.hillel._0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HillelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(com.zadoiko.hillel.hillel.HillelApplication.class, args);
+    }
+
+}
